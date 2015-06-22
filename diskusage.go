d8578f60d4ab55119ce84c3a20750c3b71b12f94
rizal <riesal@gmail.com>
2015-06-22 17:26:30 +0700

    diskusage.go 1st version

--- diskusage.go
+++ diskusage.go
@@ -16,7 +16,7 @@ func Space(path string) (total, free float64, err error) {
 	total = float64(total) / 1024 / 1024 / 1024
         totaldisk := float64(math.Ceil(total))
         free =  float64(s.Bsize) * float64(s.Bfree)
-	free =  float64(free) / 1026 / 1026 / 1026 /* aslinya 1026 */
+	free =  float64(free) / 1026 / 1026 / 1026
 	freedisk :=   float64(math.Ceil(free))
         usagedisk :=   totaldisk - freedisk
         usagepercent :=   float64(math.Ceil(usagedisk / totaldisk * 100))
